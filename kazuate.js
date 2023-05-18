c6410d33de658ccf9520d9fa02c545ed7f5e70d3
G284280622 <228428@st.takushoku-u.ac.jp>
2023-05-18 09:41:35 +0900

    課題０３

--- kazuate.js
+++ kazuate.js
@@ -4,7 +4,7 @@ console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
 let kaisu = 0;
-
+let atari = 0;
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
@@ -20,20 +20,27 @@ function hantei() {
   kaisu = kaisu+1
   console.log(kaisu+"回目の予想 : "+yoso);
   // 課題3-1: 正解判定する
-  
-  if (yoso === kotae && kaisu === 1){
-    console.log("答えは"+kotae+"です。おめでとう！");
+  if (kaisu >= 4 || atari > 1) {
+    console.log("答えは"+kotae+"でした。ゲームはすでに終わってます。");
   }
-  if (yoso === kotae && yoso !==1 && yoso<4){
-    console.log("答えは"+kotae+"です。おめでとう！");
+  if (kaisu === 1) {
+    if (kotae === yoso) {
+      console.log("正解です。おめでとう。");
+      atari++;
+    }
   }
-  else if (yoso < kotae !==1 && yoso<4) {
-    console.log("まちがい。答えはもっと大きいですよ。");
+  if (kaisu < 3) {
+    if (yoso === kotae) {
+      console.log("正解です。おめでとう。");
+    }
+    else if (yoso > kotae) {
+      console.log("まちがい。答えはもっと小さいですよ");
+    }
+    else if (yoso < kotae) {
+      console.log("まちがい。答えはもっと大きいですよ。");
+    }
   }
-  else if (yoso > kotae !==1 && yoso<4) {
-    console.log("まちがい。答えはもっと小さいですよ。");
+  if (kaisu === 3) {
+    console.log("まちがい。残念でした。答えは"+kotae+"です。");
   }
-  
-  // kotae と yoso が一致するかどうか調べて結果を出力
-  // 課題3-1における出力先はコンソール
 }
